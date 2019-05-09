5e9977dd63b18bac5bb2f4ba4f68b257d802cb6d
Abhinav Saxena <get.abhis@gmail.com>
2019-05-09 11:25:56 +0530

    Update tunning.py

--- tunning.py
+++ tunning.py
@@ -14,8 +14,21 @@ model = XGBClassifier(
  scale_pos_weight=1,
  seed=27)
 model.fit(X_train, y_train)
+#Check the accuracy on the validation data and make predictions for test data
+pred_cv = model.predict(X_test)
+
+# checking macro average F1 score
+from sklearn.metrics import f1_score
+f1_score(y_test , pred_cv, average='macro')
+
+# Prediction of test data
+Pred_test = model.predict(X_test_final)
+f1_score(y_test_final , Pred_test, average= 'macro')
+
+# we proceed with tuning and check for various parameters and how they are improving our accuracy metric.
 
 #Parameter tuning of max_depth , min_child_weight
+
 param_test1 = {
  'max_depth':range(3,10,2),
  'min_child_weight':range(1,6,2)
@@ -25,21 +38,10 @@ gsearch1 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimat
  objective= 'reg:linear', nthread=4, scale_pos_weight=1, seed=27), 
  param_grid = param_test1, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch1.fit(X_train, y_train)
-
-GridSearchCV(cv=5, error_score='raise-deprecating',
-       estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-       colsample_bytree=0.8, gamma=0, learning_rate=0.1, max_delta_step=0,
-       max_depth=5, min_child_weight=1, missing=None, n_estimators=140,
-       n_jobs=1, nthread=4, objective='reg:linear', random_state=0,
-       reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=27, silent=True,
-       subsample=0.8),
-       fit_params=None, iid=False, n_jobs=4,
-       param_grid={'max_depth': range(3, 10, 2), 'min_child_weight': range(1, 6, 2)},
-       pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
-       scoring='roc_auc', verbose=0)
 gsearch1.best_score_
 gsearch1.best_params_
-############################################## Taking different range of max_depth and Min_child_weight#########################
+
+#Taking different range of max_depth and Min_child_weight
 param_test2 = {
  'max_depth':[4,5,6],
  'min_child_weight':[4,5,6]
@@ -49,21 +51,22 @@ gsearch2 = GridSearchCV(estimator = XGBClassifier( learning_rate=0.1, n_estimato
  objective= 'reg:linear', nthread=4, scale_pos_weight=1,seed=27), 
  param_grid = param_test2, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch2.fit(X_train, y_train)
-GridSearchCV(cv=5, error_score='raise-deprecating',
-       estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-       colsample_bytree=0.8, gamma=0, learning_rate=0.1, max_delta_step=0,
-       max_depth=5, min_child_weight=2, missing=None, n_estimators=140,
-       n_jobs=1, nthread=4, objective='reg:linear', random_state=0,
-       reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=27, silent=True,
-       subsample=0.8),
-       fit_params=None, iid=False, n_jobs=4,
-       param_grid={'max_depth': [4, 5, 6], 'min_child_weight': [4, 5, 6]},
-       pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
-       scoring='roc_auc', verbose=0)
+# GridSearchCV(cv=5, error_score='raise-deprecating',
+#        estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#        colsample_bytree=0.8, gamma=0, learning_rate=0.1, max_delta_step=0,
+#        max_depth=5, min_child_weight=2, missing=None, n_estimators=140,
+#        n_jobs=1, nthread=4, objective='reg:linear', random_state=0,
+#        reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=27, silent=True,
+#        subsample=0.8),
+#        fit_params=None, iid=False, n_jobs=4,
+#        param_grid={'max_depth': [4, 5, 6], 'min_child_weight': [4, 5, 6]},
+#        pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
+#        scoring='roc_auc', verbose=0)
 gsearch2.best_params_
 gsearch2.best_score_
 
-############################ tunning further the child weight #################################################
+#tunning further the child weight
+
 param_test2b = {
  'min_child_weight':[6,8,10,12]
 }
@@ -72,21 +75,21 @@ gsearch2b = GridSearchCV(estimator = XGBClassifier( learning_rate=0.1, n_estimat
  objective= 'reg:linear', nthread=4, scale_pos_weight=1,seed=27), 
  param_grid = param_test2b, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch2b.fit(X_train , y_train)
-GridSearchCV(cv=5, error_score='raise-deprecating',
-       estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-       colsample_bytree=0.8, gamma=0, learning_rate=0.1, max_delta_step=0,
-       max_depth=4, min_child_weight=2, missing=None, n_estimators=140,
-       n_jobs=1, nthread=4, objective='reg:linear', random_state=0,
-       reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=27, silent=True,
-       subsample=0.8),
-       fit_params=None, iid=False, n_jobs=4,
-       param_grid={'min_child_weight': [6, 8, 10, 12]},
-       pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
-       scoring='roc_auc', verbose=0)
+# GridSearchCV(cv=5, error_score='raise-deprecating',
+#        estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#        colsample_bytree=0.8, gamma=0, learning_rate=0.1, max_delta_step=0,
+#        max_depth=4, min_child_weight=2, missing=None, n_estimators=140,
+#        n_jobs=1, nthread=4, objective='reg:linear', random_state=0,
+#        reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=27, silent=True,
+#        subsample=0.8),
+#        fit_params=None, iid=False, n_jobs=4,
+#        param_grid={'min_child_weight': [6, 8, 10, 12]},
+#        pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
+#        scoring='roc_auc', verbose=0)
 gsearch2b.best_params_
 gsearch2b.best_score_
 
-############################################## tune gamma #################################################
+#tune gamma
 param_test3 = {
  'gamma':[i/10.0 for i in range(0,5)]
 }
@@ -95,20 +98,21 @@ gsearch3 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimat
  objective= 'reg:linear', nthread=4, scale_pos_weight=1,seed=27), 
  param_grid = param_test3, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch3.fit(X_train, y_train)
-GridSearchCV(cv=5, error_score='raise-deprecating',
-       estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-       colsample_bytree=0.8, gamma=0, learning_rate=0.1, max_delta_step=0,
-       max_depth=4, min_child_weight=8, missing=None, n_estimators=140,
-       n_jobs=1, nthread=4, objective='reg:linear', random_state=0,
-       reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=27, silent=True,
-       subsample=0.8),
-       fit_params=None, iid=False, n_jobs=4,
-       param_grid={'gamma': [0.0, 0.1, 0.2, 0.3, 0.4]},
-       pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
-       scoring='roc_auc', verbose=0)
+# GridSearchCV(cv=5, error_score='raise-deprecating',
+#        estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#        colsample_bytree=0.8, gamma=0, learning_rate=0.1, max_delta_step=0,
+#        max_depth=4, min_child_weight=8, missing=None, n_estimators=140,
+#        n_jobs=1, nthread=4, objective='reg:linear', random_state=0,
+#        reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=27, silent=True,
+#        subsample=0.8),
+#        fit_params=None, iid=False, n_jobs=4,
+#        param_grid={'gamma': [0.0, 0.1, 0.2, 0.3, 0.4]},
+#        pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
+#        scoring='roc_auc', verbose=0)
 gsearch3.best_params_
 
-#################################################### tune subsample and col_sample ######################################
+#tune subsample and col_sample
+
 param_test4 = {
  'subsample':[i/10.0 for i in range(6,10)],
  'colsample_bytree':[i/10.0 for i in range(6,10)]
@@ -118,23 +122,23 @@ gsearch4 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimat
  objective= 'reg:linear', nthread=4, scale_pos_weight=1,seed=27), 
  param_grid = param_test4, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch4.fit(X_train, y_train)
-GridSearchCV(cv=5, error_score='raise-deprecating',
-       estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-       colsample_bytree=0.8, gamma=0.1, learning_rate=0.1,
-       max_delta_step=0, max_depth=4, min_child_weight=8, missing=None,
-       n_estimators=177, n_jobs=1, nthread=4, objective='reg:linear',
-       random_state=0, reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
-       seed=27, silent=True, subsample=0.8),
-       fit_params=None, iid=False, n_jobs=4,
-       param_grid={'subsample': [0.6, 0.7, 0.8, 0.9], 'colsample_bytree': [0.6, 0.7, 0.8, 0.9]},
-       pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
-       scoring='roc_auc', verbose=0)
-gsearch4.best_score_
+# GridSearchCV(cv=5, error_score='raise-deprecating',
+#        estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#        colsample_bytree=0.8, gamma=0.1, learning_rate=0.1,
+#        max_delta_step=0, max_depth=4, min_child_weight=8, missing=None,
+#        n_estimators=177, n_jobs=1, nthread=4, objective='reg:linear',
+#        random_state=0, reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
+#        seed=27, silent=True, subsample=0.8),
+#        fit_params=None, iid=False, n_jobs=4,
+#        param_grid={'subsample': [0.6, 0.7, 0.8, 0.9], 'colsample_bytree': [0.6, 0.7, 0.8, 0.9]},
+#        pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
+#        scoring='roc_auc', verbose=0)
+# gsearch4.best_score_
 gsearch4.best_params_
 
-####################################### tuning subsample  and colsample ############################
+#tuning subsample  and colsample
 # Will increment subsample and colsample by step of 0.05
-.1
+
 param_test5 = {
  'subsample':[i/100.0 for i in range(75,90,5)],
  'colsample_bytree':[i/100.0 for i in range(75,90,5)]
@@ -144,20 +148,20 @@ gsearch5 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimat
  objective= 'binary:logistic', nthread=4, scale_pos_weight=1,seed=27), 
  param_grid = param_test5, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch5.fit(X_train, y_train)
-GridSearchCV(cv=5, error_score='raise-deprecating',
-       estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-       colsample_bytree=0.8, gamma=0.1, learning_rate=0.1,
-       max_delta_step=0, max_depth=4, min_child_weight=8, missing=None,
-       n_estimators=177, n_jobs=1, nthread=4, objective='binary:logistic',
-       random_state=0, reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
-       seed=27, silent=True, subsample=0.8),
-       fit_params=None, iid=False, n_jobs=4,
-       param_grid={'subsample': [0.75, 0.8, 0.85], 'colsample_bytree': [0.75, 0.8, 0.85]},
-       pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
-       scoring='roc_auc', verbose=0)
+# GridSearchCV(cv=5, error_score='raise-deprecating',
+#        estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#        colsample_bytree=0.8, gamma=0.1, learning_rate=0.1,
+#        max_delta_step=0, max_depth=4, min_child_weight=8, missing=None,
+#        n_estimators=177, n_jobs=1, nthread=4, objective='binary:logistic',
+#        random_state=0, reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
+#        seed=27, silent=True, subsample=0.8),
+#        fit_params=None, iid=False, n_jobs=4,
+#        param_grid={'subsample': [0.75, 0.8, 0.85], 'colsample_bytree': [0.75, 0.8, 0.85]},
+#        pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
+#        scoring='roc_auc', verbose=0)
 gsearch5.best_params_
 
-################################################# tune regularization parameter ###############################
+#tune regularization parameter
 tree=0.6,
 param_test6 = {
  'reg_alpha':[1e-5, 1e-2, 0.1, 1, 100]
@@ -167,20 +171,21 @@ gsearch6 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimat
  objective= 'reg:linear', nthread=4, scale_pos_weight=1,seed=27), 
  param_grid = param_test6, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
 gsearch6.fit(X_train, y_train)
-GridSearchCV(cv=5, error_score='raise-deprecating',
-       estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-       colsample_bytree=0.6, gamma=0.1, learning_rate=0.1,
-       max_delta_step=0, max_depth=4, min_child_weight=8, missing=None,
-       n_estimators=177, n_jobs=1, nthread=4, objective='reg:linear',
-       random_state=0, reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
-       seed=27, silent=True, subsample=0.9),
-       fit_params=None, iid=False, n_jobs=4,
-       param_grid={'reg_alpha': [1e-05, 0.01, 0.1, 1, 100]},
-       pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
-       scoring='roc_auc', verbose=0)
+# GridSearchCV(cv=5, error_score='raise-deprecating',
+#        estimator=XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#        colsample_bytree=0.6, gamma=0.1, learning_rate=0.1,
+#        max_delta_step=0, max_depth=4, min_child_weight=8, missing=None,
+#        n_estimators=177, n_jobs=1, nthread=4, objective='reg:linear',
+#        random_state=0, reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
+#        seed=27, silent=True, subsample=0.9),
+#        fit_params=None, iid=False, n_jobs=4,
+#        param_grid={'reg_alpha': [1e-05, 0.01, 0.1, 1, 100]},
+#        pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
+#        scoring='roc_auc', verbose=0)
 gsearch6.best_params_
 gsearch6.best_score_
 
+# After tunning for this particular data I get tunned parameters as max_depth = 6 , gamma = 0 , colsample_bytree = 0.9, 
 # fit model and find accuracy 
 xgb3 = XGBClassifier(
  learning_rate =0.2,
@@ -198,3 +203,10 @@ xgb3 = XGBClassifier(
 xgb3.fit(X_train , y_train)
 pred_cv = xgb3.predict(X_test)
 
+# checking macro average F1 score
+from sklearn.metrics import f1_score
+f1_score(y_test , pred_cv, average='macro')
+
+# Prediction of test data
+Pred_test = model.predict(X_test_final)
+f1_score(y_test_final , Pred_test, average= 'macro')
